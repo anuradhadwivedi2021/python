ed475df1160639f5510ad325da6cbf74e387e85f
anuradha dilip dwivedi <anuradha.dwivedi2021@gmail.com>
2025-04-05 23:42:24 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,8 @@
+# intializing a dicitonary
+user_data={a}
+user_data['name']=input("enter your name:")
+user_data['age']=int(input("enter your age:"))
+user_data['height']=float(input("enter your height:"))
+user_data['student']=input("are u student(yes/no):")
+# print the input from us
+print(user_data)
